9621e0479df4cb40eed1e7741932bd9d496dc734
kevingdf68 <133802355+kevingdf68@users.noreply.github.com>
2026-06-08 23:21:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def set_background(image_file="fundo.png"):
 
 st.set_page_config(
     page_title="Triagem de Risco Cardiovascular",
-    page_icon="❤️",
+    page_icon="",
     layout="centered"
 )
 
